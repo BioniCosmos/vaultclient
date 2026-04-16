89c0dc09694242ed0c1a66212201c83bf08b3a6c
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2026-04-16 17:00:32 +0800

    Implement secure communication

--- src/main.zig
+++ src/main.zig
@@ -15,10 +15,14 @@ const c = @cImport({
     @cInclude("tomcrypt.h");
 });
 
+var session_key: [64]u8 = undefined;
+
 pub fn main() !void {
     var gpa = heap.DebugAllocator(.{}).init;
     defer debug.assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
+
+    try initCrypto();
 
     var in_buf: [1024]u8 = undefined;
     var reader = fs.File.stdin().reader(&in_buf);
@@ -26,55 +30,222 @@ pub fn main() !void {
 
     while (true) {
         const raw = try in.take(try in.takeInt(u32, .little));
-        log.info("receive: {s}\n", .{raw});
+        log.info("receive: {s}", .{raw});
+
         const wrapper = try json.parseFromSlice(ReceiveWrapper, allocator, raw, .{});
-        if (mem.eql(u8, wrapper.value.message.command, "setupEncryption")) {
-            try setupEncryption(allocator, wrapper.value);
+        defer wrapper.deinit();
+
+        if (wrapper.value.message.object.contains("command")) {
+            const message = try json.parseFromValue(Receive, allocator, wrapper.value.message, .{});
+            defer message.deinit();
+
+            if (mem.eql(u8, message.value.command, "setupEncryption")) {
+                try setupEncryption(allocator, &message.value, wrapper.value.appId);
+            }
+        } else {
+            const encrypted = try json.parseFromValue(
+                EncString.Fields,
+                allocator,
+                wrapper.value.message,
+                .{ .ignore_unknown_fields = true },
+            );
+            defer encrypted.deinit();
+
+            const message = try decryptMessage(allocator, &encrypted.value, &session_key);
+            defer message.deinit();
+
+            if (mem.eql(u8, message.value.command, "getBiometricsStatus")) {
+                const encrypted_send = try encryptMessage(allocator, .{
+                    .command = message.value.command,
+                    .messageId = message.value.messageId,
+                    .response = .{ .integer = 0 },
+                    .timestamp = std.time.milliTimestamp(),
+                }, &session_key);
+                defer encrypted_send.deinit();
+
+                try sendMessage(allocator, .{
+                    .appId = wrapper.value.appId,
+                    .messageId = message.value.messageId,
+                    .message = encrypted_send.fields,
+                });
+            }
         }
-        wrapper.deinit();
     }
 }
 
-const ReceiveWrapper = struct { message: Receive, appId: []const u8 };
+const EncString = struct {
+    const Self = @This();
+    const Fields = struct {
+        encryptedString: []const u8,
+        encryptionType: u8 = 2,
+        data: []const u8,
+        iv: []const u8,
+        mac: []const u8,
+    };
+
+    fields: Fields,
+    arena: ?heap.ArenaAllocator = null,
+
+    fn init(allocator: mem.Allocator, v: Self) !Self {
+        const arena = heap.ArenaAllocator.init(allocator);
+        const alloc = arena.allocator();
+        return .{
+            .encryptedString = try alloc.dupe(u8, v.encryptedString),
+            .encryptionType = v.encryptionType,
+            .data = try alloc.dupe(u8, v.data),
+            .iv = try alloc.dupe(u8, v.iv),
+            .mac = try alloc.dupe(u8, v.iv),
+            .arena = arena,
+        };
+    }
+
+    fn deinit(self: *const Self) void {
+        self.arena.?.deinit();
+    }
+};
+
+const ReceiveWrapper = struct { message: json.Value, appId: []const u8 };
 
 const Receive = struct {
     command: []const u8,
     messageId: i32,
-    userId: ?[]const u8,
-    timestamp: ?u64,
-    publicKey: ?[]const u8,
+    userId: []const u8,
+    timestamp: i64,
+    publicKey: ?[]const u8 = null,
 };
 
 const Send = struct {
     appId: []const u8,
-    messageId: ?i32,
-    command: ?[]const u8,
-    sharedSecret: ?[]const u8,
-    // message
+    messageId: i32,
+    command: ?[]const u8 = null,
+    sharedSecret: ?[]const u8 = null,
+    message: ?EncString.Fields = null,
 };
 
-fn setupEncryption(allocator: mem.Allocator, wrapper: ReceiveWrapper) !void {
-    const public_key = try allocator.alloc(
-        u8,
-        try base64.standard.Decoder.calcSizeForSlice(wrapper.message.publicKey.?),
-    );
+const SendInner = struct { command: []const u8, messageId: i32, response: json.Value, timestamp: i64 };
+
+fn setupEncryption(allocator: mem.Allocator, message: *const Receive, appId: []const u8) !void {
+    const public_key = try base64Decode(allocator, message.publicKey.?);
     defer allocator.free(public_key);
-    try base64.standard.Decoder.decode(public_key, wrapper.message.publicKey.?);
 
     var secret: [64]u8 = undefined;
     crypto.random.bytes(&secret);
+    session_key = secret;
+    log.debug("key: {s}", .{fmt.bytesToHex(secret, .lower)});
 
     var ciphertext_store: [512]u8 = undefined;
     const ciphertext = try rsaEncrypt(&secret, public_key, &ciphertext_store);
 
-    const encoded = try allocator.alloc(u8, base64.standard.Encoder.calcSize(ciphertext.len));
+    const encoded = try base64Encode(allocator, ciphertext);
     defer allocator.free(encoded);
+
     try sendMessage(allocator, .{
-        .appId = wrapper.appId,
+        .appId = appId,
         .messageId = -1,
-        .command = wrapper.message.command,
-        .sharedSecret = base64.standard.Encoder.encode(encoded, ciphertext),
+        .command = message.command,
+        .sharedSecret = encoded,
     });
+}
+
+fn decryptMessage(
+    allocator: mem.Allocator,
+    encrypted: *const EncString.Fields,
+    key: *const [64]u8,
+) !json.Parsed(Receive) {
+    const enc_key = key[0..32];
+
+    const iv = try base64Decode(allocator, encrypted.iv);
+    defer allocator.free(iv);
+
+    const message = try base64Decode(allocator, encrypted.data);
+    defer allocator.free(message);
+
+    var err = c.CRYPT_OK;
+    var cbc: c.symmetric_CBC = undefined;
+    err = c.cbc_start(c.find_cipher("aes"), iv.ptr, enc_key, enc_key.len, 14, &cbc);
+    if (err != c.CRYPT_OK) {
+        log.err("decryptMessage: {s}", .{c.error_to_string(err)});
+        return error.DecryptMessage;
+    }
+
+    err = c.cbc_decrypt(message.ptr, @constCast(message.ptr), message.len, &cbc);
+    if (err != c.CRYPT_OK) {
+        log.err("decryptMessage: {s}", .{c.error_to_string(err)});
+        return error.DecryptMessage;
+    }
+
+    err = c.cbc_done(&cbc);
+    if (err != c.CRYPT_OK) {
+        log.err("decryptMessage: {s}", .{c.error_to_string(err)});
+        return error.DecryptMessage;
+    }
+
+    const unpadded = try unpadPKCS7(message);
+    log.info("decrypted: {s}", .{unpadded});
+
+    return try json.parseFromSlice(Receive, allocator, unpadded, .{ .allocate = .alloc_always });
+}
+
+fn encryptMessage(allocator: mem.Allocator, message: SendInner, key: *const [64]u8) !EncString {
+    const enc_key = key[0..32];
+    const auth_key = key[32..];
+
+    var iv: [16]u8 = undefined;
+    crypto.random.bytes(&iv);
+
+    const encoded = try fmt.allocPrint(allocator, "{f}", .{json.fmt(message, .{})});
+    defer allocator.free(encoded);
+
+    const bytes = try padPKCS7(allocator, encoded);
+    defer allocator.free(bytes);
+
+    var err = c.CRYPT_OK;
+    var cbc: c.symmetric_CBC = undefined;
+    err = c.cbc_start(c.find_cipher("aes"), &iv, enc_key, enc_key.len, 14, &cbc);
+    if (err != c.CRYPT_OK) {
+        log.err("decryptMessage: {s}", .{c.error_to_string(err)});
+        return error.DecryptMessage;
+    }
+
+    err = c.cbc_encrypt(bytes.ptr, @constCast(bytes.ptr), bytes.len, &cbc);
+    if (err != c.CRYPT_OK) {
+        log.err("decryptMessage: {s}", .{c.error_to_string(err)});
+        return error.DecryptMessage;
+    }
+
+    err = c.cbc_done(&cbc);
+    if (err != c.CRYPT_OK) {
+        log.err("decryptMessage: {s}", .{c.error_to_string(err)});
+        return error.DecryptMessage;
+    }
+
+    const HmacSha256 = crypto.auth.hmac.sha2.HmacSha256;
+    var mac: [HmacSha256.mac_length]u8 = undefined;
+    var hmac = HmacSha256.init(auth_key);
+    hmac.update(&iv);
+    hmac.update(bytes);
+    hmac.final(&mac);
+
+    var arena = heap.ArenaAllocator.init(allocator);
+    const alloc = arena.allocator();
+
+    const encoded_iv = try base64Encode(alloc, &iv);
+    const encoded_message = try base64Encode(alloc, bytes);
+    const encoded_mac = try base64Encode(alloc, &mac);
+
+    return .{
+        .fields = .{
+            .encryptedString = try fmt.allocPrint(
+                alloc,
+                "2.{s}|{s}|{s}",
+                .{ encoded_iv, encoded_message, encoded_mac },
+            ),
+            .data = encoded_message,
+            .iv = encoded_iv,
+            .mac = encoded_mac,
+        },
+        .arena = arena,
+    };
 }
 
 var stdout_buf: [1024]u8 = undefined;
@@ -95,15 +266,6 @@ fn sendMessage(allocator: mem.Allocator, message: Send) !void {
 }
 
 fn rsaEncrypt(plaintext: []const u8, public_key: []const u8, ciphertext: []u8) ![]const u8 {
-    if (c.register_prng(&c.sprng_desc) == -1) {
-        return error.RegisterPRNG;
-    }
-
-    c.ltc_mp = c.tfm_desc;
-    if (c.register_hash(&c.sha1_desc) == -1) {
-        return error.RegisterHash;
-    }
-
     var err = c.CRYPT_OK;
     var key: c.rsa_key = undefined;
     err = c.rsa_import(public_key.ptr, public_key.len, &key);
@@ -131,4 +293,56 @@ fn rsaEncrypt(plaintext: []const u8, public_key: []const u8, ciphertext: []u8) !
     }
 
     return ciphertext[0..ciphertext_len];
+}
+
+fn unpadPKCS7(data: []const u8) ![]const u8 {
+    if (data.len == 0) {
+        return data;
+    }
+
+    const len = data[data.len - 1];
+    if (len == 0 or len > data.len) {
+        return error.InvalidPadding;
+    }
+
+    const start = data.len - len;
+    if (!mem.allEqual(u8, data[start..], len)) {
+        return error.InvalidPadding;
+    }
+    return data[0..start];
+}
+
+fn padPKCS7(allocator: mem.Allocator, data: []const u8) ![]const u8 {
+    const len = 16 - data.len % 16;
+    const padded = try allocator.alloc(u8, data.len + len);
+    @memcpy(padded[0..data.len], data);
+    @memset(padded[data.len..], @intCast(len));
+    return padded;
+}
+
+fn base64Encode(allocator: mem.Allocator, raw: []const u8) ![]const u8 {
+    const encoded = try allocator.alloc(u8, base64.standard.Encoder.calcSize(raw.len));
+    return base64.standard.Encoder.encode(encoded, raw);
+}
+
+fn base64Decode(allocator: mem.Allocator, encoded: []const u8) ![]const u8 {
+    const raw = try allocator.alloc(u8, try base64.standard.Decoder.calcSizeForSlice(encoded));
+    try base64.standard.Decoder.decode(raw, encoded);
+    return raw;
+}
+
+fn initCrypto() !void {
+    c.ltc_mp = c.tfm_desc;
+
+    if (c.register_prng(&c.sprng_desc) == -1) {
+        return error.RegisterPRNG;
+    }
+
+    if (c.register_hash(&c.sha1_desc) == -1) {
+        return error.RegisterHash;
+    }
+
+    if (c.register_cipher(&c.aes_desc) == -1) {
+        return error.RegisterCipher;
+    }
 }
